9902b4128e9d62e297f1bbeba0db82728906d87b
adnan <adnankaka.0092@gmail.com>
2025-06-27 02:52:26 +0500

    commit

--- components/home/Estimate.js
+++ components/home/Estimate.js
@@ -24,7 +24,7 @@ const Estimate = () => {
   }, []);
 
   return (
-    <section className="relative pt-10 lg:py-10 order-2">
+    <section className="relative pt-16 lg:py-10 order-2">
       <div className="container">
         <div className="row">
           <div className="w-full">
