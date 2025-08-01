707cc097afbb4318c1b30fc1682418391b09f215
nabeelzwei <nabeel.yousaf@zweidevs.com>
2025-08-01 20:22:28 +0500

    update config url

--- app/_libs/config.js
+++ app/_libs/config.js
@@ -6,8 +6,8 @@ export function pickByEnv({ dev, prod, override, fallback }) {
 }
 
 export const GRAPHQL_ENDPOINT =
-  process.env.NEXT_PUBLIC_GRAPHQL_PRODUCTION_ENDPOINT
-  || 'http://graphql-api-production.up.railway.app';
+  "http://graphql-api-production.up.railway.app/graphql"
+  || 'http://graphql-api-production.up.railway.app/graphql';
 
 export const SITE_URL = pickByEnv({
   dev: process.env.NEXT_PUBLIC_SITE_URL_DEV,
