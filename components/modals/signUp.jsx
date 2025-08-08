9c1e029fd4ecef70887ebde6322040dad25982e4
Agent1190 <asghar.hammad110@gmail.com>
2025-08-09 00:05:28 +0500

    update

--- components/modals/signUp.jsx
+++ components/modals/signUp.jsx
@@ -120,7 +120,7 @@ const SignUp = ({
         >
           <img src="/images/fav.png" alt="" className="max-w-20 lg:max-w-24" />
           <h2 className="text-center font-bold text-[22px] lg:text-[24px] leading-[1.3] text-black">
-            Enter your email to receive your estimate
+            Enter your email to receive your estimate 
             {/* &amp; we'll keep it sent to your email as well */}
             {/* Keep These Prices &amp; Get Details of This Project Sent To Your Email */}
           </h2>
