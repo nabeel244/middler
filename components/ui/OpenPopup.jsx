3d088a27bec2f7b115deeddbe66a84f4cbaa49b0
adnan <adnankaka.0092@gmail.com>
2025-08-13 00:42:59 +0500

    commit

--- components/ui/OpenPopup.jsx
+++ components/ui/OpenPopup.jsx
@@ -32,7 +32,7 @@ const OpenPopup = ({ showPopUp, setShowPopUp }) => {
           animate={{ y: 0, opacity: 1 }}
           exit={{ y: 120, opacity: 0 }}
           transition={{ type: "spring", stiffness: 260, damping: 24 }}
-          className="fixed top-15 lg:top-35 right-0 rounded-lg -rotate-90 origin-bottom-right bg-black text-white hover:shadow-[0_0_20px] shadow-blue-600/40 hover:-translate-y-2 z-[999] px-4 lg:px-5 py-1.5 lg:py-2 transition-all duration-300 ease-in-out cursor-pointer"
+          className="fixed top-15 lg:top-auto lg:bottom-3 lg:right-3 right-0 rounded-lg -rotate-90 lg:rotate-0 origin-bottom-right bg-black text-white hover:shadow-[0_0_20px] shadow-blue-600/40 hover:-translate-y-2 z-[999] px-4 lg:px-5 py-1.5 lg:py-2 transition-all duration-300 ease-in-out cursor-pointer"
         >
           10% Off Five Star Painting
         </motion.button>
