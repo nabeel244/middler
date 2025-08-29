4ddfc7baf6a3885721cec3c867140bbf04b9d5e8
Shoaib Ansari <shoaibansari824@gmail.com>
2025-08-30 01:45:09 +0500

    +gtags script

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Poppins } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import Providers from './Providers';
 
@@ -13,6 +14,18 @@ export default function RootLayout({ children }) {
     <html lang="en">
     <meta name="google-site-verification" content="cXkEsP_rWBJOqS_x8q9XsG3hfwXWJgC9Wvnt93V-PcU" />
       <body className={`${poppins.variable} font-sans`}>
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-T72TYPR1EE"
+            strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-T72TYPR1EE');
+              `}
+        </Script>
         <Providers>{children}</Providers>
       </body>
     </html>
