70cf1c22ce62e50ad4fa501292f081db717c2d79
Muhammad Waleed <muhammad.waleed@diginers.com>
2025-12-24 10:53:01 +0500

    Update URls

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/blog/:slug*',
+        destination: 'https://primary-production-bf78.up.railway.app/:slug*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
